79abc0782be09503e2e730b929e50d3170929649
Smita sriya Sarangi <smitassarangi123@gmail.com>
2024-12-07 02:31:23 +0000

    app.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 // src/App.js
 import React from 'react';
-import { Box } from '@mui/material'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Box } from '@mui/material';
+import sunsetTheme from './theme/sunset';
+
+
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import ParkingLots from './pages/ParkingLots';
-import Reservations from './pages/Reservation';
-import User from './pages/User';
+import HomePage from './components/Homepage';
+import Footer from './components/Footer';
+import Footer from './components/Footer';
+import Footer from './components/Footer';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/parking-lots" element={<ParkingLots />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/login" element={<User />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={sunsetTheme}>
+      <CssBaseline />
+      <Router>
+        <Box display="flex" flexDirection="column" minHeight="100vh">
+          <Navbar />
+          <Box component="main" flexGrow={1} py={3}>
+            <Routes>
+              <Route exact path='/' element={<HomePage />} />
+              <Route path='/book-list' element={<ShowBookList />} />
+              <Route path='/create-book' element={<CreateBook />} />
+              <Route path='/edit-book/:id' element={<UpdateBookInfo />} />
+              <Route path='/show-book/:id' element={<ShowBookDetails />} />
+              <Route path='/notes/*' element={<NotesPage />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </Box>
+      </Router>
+    </ThemeProvider>
   );
 };
 
