95bec66572bb677ede8432dba07e6eed78f8ac51
Smita sriya Sarangi <159314912+smita0t7@users.noreply.github.com>
2024-12-08 19:48:41 -0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'; // import Router
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import sunsetTheme from './theme/sunset'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
