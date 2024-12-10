411b51c37481e0762a911053970a566a3f4caca4
Smita sriya Sarangi <159314912+smita0t7@users.noreply.github.com>
2024-12-10 20:33:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import connectDB from './config/db';
 import parkingLotRoutes from './routes/parkingLotRoutes.js'; // Correct import path
+import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+connectDB();
 
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/parking-system', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('MongoDB connected successfully!'))
-    .catch((err) => console.error('Error connecting to MongoDB:', err));
+app.use(cors());
+app.use('/routes/pakingLotRoutes');
 
 // Use parking lot routes
 app.use('/parkinglots', parkingLotRoutes); // Register routes
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
+const port = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
