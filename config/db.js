e7cce3e081d54bb51008b6c7dfb438e8c2662b99
Smita sriya Sarangi <smitassarangi123@gmail.com>
2025-02-11 14:44:34 +0000

    backend updated

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose');
 const connectDB = async () => {
     try {
         // await mongoose.connect('mongodb://localhost:27017/mydatabase');
-        await mongoose.connect('mongodb+srv://alibha04:alibha04@cluster1.ug2xy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
+        await mongoose.connect('mongodb+srv://smitasgi34:smitasgi34@smita.3qqxv.mongodb.net/?retryWrites=true&w=majority&appName=Smita');
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Failed to connect to MongoDB', err);
@@ -11,21 +11,5 @@ const connectDB = async () => {
     }
 };
 
-module.exports = connectDB
+module.exports = connectDB;
 
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect('mongodb+srv://alibha04:alibha04@cluster1.ug2xy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
