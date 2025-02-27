129b0041e91e70ce1154ea1ccd46056b0b99cdab
Smita sriya Sarangi <smitassarangi123@gmail.com>
2025-02-27 15:19:34 +0000

    comment lines are deleted

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,32 +1,3 @@
-// // src/components/Footer.js
-// import React from 'react';
-// import { Box, Typography } from '@mui/material';
-
-// const Footer = () => (
-//   <Box 
-//     component="footer" 
-//     sx={{
-//       bottom: 0,
-//       bgcolor: 'background.paper',
-//       color: 'text.secondary',
-//       py: 4,
-//       width: '100%',
-//       textAlign: 'center',
-//       maxHeight:'100px',
-//       borderTop: `1px solid ${theme => theme.palette.primary.main}`, // Optional: add a top border
-//     }}
-//   >
-//     <Typography variant="h6" gutterBottom>
-//       Built with ❤️ SMITA
-//     </Typography>
-//     <Typography variant="body2" sx={{ mt: 2 }}>
-//       © {new Date().getFullYear()} Smita  | All Rights Reserved
-//     </Typography>
-//   </Box>
-// );
-
-// export default Footer;
-
 // src/components/Footer.js
 import React from 'react';
 import { Box, Typography } from '@mui/material';
